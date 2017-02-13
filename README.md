29bc90006df40637dc727dbf606c6b183c2e20be
Steven Vascellaro <S.Vascellaro@gmail.com>
2017-02-13 13:08:01 -0500

    Initial project setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Virtual Game Shelf - A Game Collection Organizer
+Virtual Game Shelf is a desktop Java application to organize your game library
+in a single convenient location. It strives to implement the best features
+from services like [Backloggery](http://www.backloggery.com/), without
+the need for an internet connection.
+
+Code contributions should follow [Google's Java Style Guide](https://google.github.io/styleguide/javaguide.html)
+
+## Building Virtual Game Shelf
+
+Virtual Game Shelf was compiled using [Eclipse](https://eclipse.org/) with
+the [e(fx)clipse](https://www.eclipse.org/efxclipse/install.html) addon to allow the use
+of the JavaFX library.
---
+++ src/edu/adelphi/svascellaro/virtualGameShelf/VirtualGameShelf.java
@@ -0,0 +1,31 @@
+package edu.adelphi.svascellaro.virtualGameShelf;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class VirtualGameShelf extends Application {
+	public static void main(String[] args) {
+		// Automatic VM reset, thanks to Joseph Rachmuth.
+        try {
+            launch(args);
+            System.exit(0);
+        }
+        catch (Exception error) {
+            error.printStackTrace();
+            System.exit(0);
+        }
+	}
+	
+	@Override
+	public void start(Stage mainStage) throws Exception {
+		mainStage.setTitle("Virtual Game Shelf");
+		
+		BorderPane root = new BorderPane();
+		Scene mainScene = new Scene(root);
+		mainStage.setScene(mainScene);
+		
+		mainStage.show();
+	}
+}
