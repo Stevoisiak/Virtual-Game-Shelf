cf28048b3e52e1ee243fa1f903195b2b5929e8af
Steven Vascellaro <S.Vascellaro@gmail.com>
2017-02-16 11:57:16 -0500

    Use Tab indentation
    Use tabs instead of spaces for indentation

--- src/VirtualGameShelf.java
+++ src/VirtualGameShelf.java
@@ -8,24 +8,24 @@ import javafx.stage.Stage;
 public class VirtualGameShelf extends Application {
 	public static void main(String[] args) {
 		// Automatic VM reset, thanks to Joseph Rachmuth.
-        try {
-            launch(args);
-            System.exit(0);
-        }
-        catch (Exception error) {
-            error.printStackTrace();
-            System.exit(0);
-        }
+		try {
+			launch(args);
+			System.exit(0);
+		}
+		catch (Exception error) {
+			error.printStackTrace();
+			System.exit(0);
+		}
 	}
-	
+
 	@Override
 	public void start(Stage mainStage) throws Exception {
 		mainStage.setTitle("Virtual Game Shelf");
-		
+
 		BorderPane root = new BorderPane();
 		Scene mainScene = new Scene(root);
 		mainStage.setScene(mainScene);
-		
+
 		mainStage.show();
 	}
 }
