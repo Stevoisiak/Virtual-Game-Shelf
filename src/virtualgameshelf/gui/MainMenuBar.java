a9934c9f2de2d4939f9357e6f6da2ed5858d9edc
Steven Vascellaro <S.Vascellaro@gmail.com>
2017-04-08 14:10:04 -0400

    GameShelf: Rename `displayGameConsoles()` to `refreshGameListDisplay()`

--- src/virtualgameshelf/gui/MainMenuBar.java
+++ src/virtualgameshelf/gui/MainMenuBar.java
@@ -92,7 +92,7 @@ public class MainMenuBar extends MenuBar {
     /** Clears current gameList. */
     public void onNew() {
         VirtualGameShelf.gameList = new GameList();
-        VirtualGameShelf.displayGameConsoles();
+        VirtualGameShelf.refreshGameListDisplay();
     }
 
     /** Display prompt to load gameList from file. */
