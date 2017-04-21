3c4ceff54f5efd51becea718cdc583f93d5840d9
Steven Vascellaro <S.Vascellaro@gmail.com>
2017-04-21 17:25:57 -0400

    Persistance: Add code comments

--- src/virtualgameshelf/backend/persistance/SteamCommunityGameImporter.java
+++ src/virtualgameshelf/backend/persistance/SteamCommunityGameImporter.java
@@ -18,6 +18,24 @@ import java.net.URL;
  */
 
 public class SteamCommunityGameImporter {
+    /**
+     * Imports games from Steam community profile. Profile must be set to public
+     * in 'My Privacy Settings'.
+     * <p>
+     * Games are retrieved via an XML file located by appending '&xml=1' to the
+     * URL for a profile's gamelist.
+     *
+     * @param id
+     *            Steam profile ID. (ie: https://steamcommunity.com/id/[ID])
+     */
+
+    // TODO: Will not work for accounts that have not set up a profile ID.
+    //       For example, https://steamcommunity.com/profiles/76561198043604940
+
+    // Steven: Possibly use https://github.com/xPaw/SteamID.php to convert
+    //         between URL types? The conversion could happen in the GUI level, so this
+    //         file just receives the profile ID
+
     public void steamCommunityAddGames(String id) {
         try {
             String url = "http://steamcommunity.com/id/" + id + "/games/?tab=all&xml=1";
