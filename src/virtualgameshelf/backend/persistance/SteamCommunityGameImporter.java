e0e6d025c79e64637b74343a6ee33917b9a4207d
Morgan Abrams <morganabrams@mail.adelphi.edu>
2017-04-22 12:31:14 -0400

    Hours Fix
    The over 999 hours not reading was fixed.

--- src/virtualgameshelf/backend/persistance/SteamCommunityGameImporter.java
+++ src/virtualgameshelf/backend/persistance/SteamCommunityGameImporter.java
@@ -32,6 +32,21 @@ public class SteamCommunityGameImporter {
     // TODO: Will not work for accounts that have not set up a profile ID.
     //       For example, https://steamcommunity.com/profiles/76561198043604940
 
+	/* DOM vs SAX
+	 *
+	 * DOM (Document Object Model) Parsing
+	 *  - loads whole XML document in memory
+	 *  - is slower the larger the XML file
+	 *
+	 *
+	 * SAX (Simple API for XML) Parsing
+	 *  - loads small part of the XML file in memory
+	 *  - faster then DOM for large XML file cause requires less memory
+	 *  - better for unknown file size
+	 *
+	 * DOM winner
+	 */
+
     // Steven: Possibly use https://github.com/xPaw/SteamID.php to convert
     //         between URL types? The conversion could happen in the GUI level, so this
     //         file just receives the profile ID
@@ -64,7 +79,7 @@ public class SteamCommunityGameImporter {
 
                     String name = "";
                     name = (eElement.getElementsByTagName("name").item(0).getTextContent());
-                    //System.out.println(name);
+                    //System.out.println(name); //Used for finding which game causes the break
                     /*
                      * This is apparently Unneeded
                      * name = name.substring(10, name.length() - 4);
@@ -73,7 +88,14 @@ public class SteamCommunityGameImporter {
                     int hours = 0;
                     if(eElement.getElementsByTagName("hoursOnRecord").getLength() > 0){
                     	String stringHours = (eElement.getElementsByTagName("hoursOnRecord").item(0).getTextContent());
-                    	// System.out.println(stringHours);
+                    	//System.out.println(stringHours); //Used for finding which games hours cause the break
+
+                    	//Checking to see if the hours is going to have a comma in it and if so, removes the comma
+                    	if(stringHours.length() >= 7)
+                    	{
+                    		stringHours = stringHours.substring(0,stringHours.indexOf(",")) + stringHours.substring(stringHours.indexOf(",")+1);
+                    		//System.out.println(stringHours); //Used to test if the comma was correctly removed
+                    	}
                         hours = (int) Math.round(Double.parseDouble(stringHours));
                     }
 
@@ -82,13 +104,16 @@ public class SteamCommunityGameImporter {
                         completion = "Unplayed";
                     }
 
-                    Game game = new Game();
-                    game.setName(name);
-                    game.setSystem("PC");
-                    game.setHours(hours);
-                    game.setCompletion(completion);
-                    game.setRating(0);
-                    GameShelf.gameList.addGame(game);
+                   //Makes sure we are actually creating a game that has a name.
+                    if(!(name.equals(""))) {
+	                    Game game = new Game();
+	                    game.setName(name);
+	                    game.setSystem("PC");
+	                    game.setHours(hours);
+	                    game.setCompletion(completion);
+	                    game.setRating(0);
+	                    GameShelf.gameList.addGame(game);
+                    }
 
                 }
             }
